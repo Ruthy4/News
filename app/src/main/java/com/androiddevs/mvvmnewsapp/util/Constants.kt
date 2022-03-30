28a1b6a8e8aaa61aa85dc7960b354b47952e3572
Unoka Ruth <33810711+Ruthy4@users.noreply.github.com>
2022-03-30 18:34:54 +0100

    paginate news response

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,10 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants {
+    companion object {
+        const val API_KEY = "bd61dec1c0194669ae49ecc2650e3aa7"
+        const val BASE_URL = "https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
+        const val QUERY_PAGE_SIZE = 20
+    }
+}
